d0738e86c69615f500fba3cb891ebdf1d9d17ee1
Simon Breiter <hello@simonbreiter.com>
2018-05-22 23:52:23 +0200

    test: test hit probability with no reroll

--- test/hitProbability.test.js
+++ test/hitProbability.test.js
@@ -3,3 +3,27 @@ import { hitProbability } from '../src/hitProbability'
 test('function exists', () => {
   expect(hitProbability()).toBeDefined()
 })
+
+test('hit probability with no reroll', () => {
+  const props = {
+    model: {
+      melee: {
+        skill: 3
+      },
+      ballistic: {
+        skill: 3
+      }
+    },
+    reroll: {
+      melee: 'reroll-none',
+      ballistic: 'reroll-none'
+    }
+  }
+  const expected = {
+    melee: 0.667,
+    ballistic: 0.667
+  }
+
+  expect(hitProbability(props).melee).toBeCloseTo(expected.melee)
+  expect(hitProbability(props).ballistic).toBeCloseTo(expected.ballistic)
+})
