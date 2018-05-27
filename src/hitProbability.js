6466f081b3d24f6ea678e6beb98f6913eddf1fd0
Simon Breiter <hello@simonbreiter.com>
2018-05-28 01:10:38 +0200

    refactor: use consistent parameter names

--- src/hitProbability.js
+++ src/hitProbability.js
@@ -1,22 +1,22 @@
 /**
  * Calculate hit probability
- * @param {object} props - the property object
+ * @param {object} props - property object
  * @param {object} props.model - model object
  * @param {object} props.model.melee - melee object
  * @param {number} props.model.melee.skill - melee skill
  * @param {object} props.model.ballistic - model object
  * @param {number} props.model.ballistic.skill - model object
- * @param {object} [props.reroll] - reroll object
- * @param {string} [props.reroll.melee] - reroll option for melee attacks
- * @param {string} [props.reroll.ballistic] - reroll option for ballistic attacks
+ * @param {object} [props.hitReroll] - hit reroll object
+ * @param {string} [props.hitReroll.melee] - hit reroll option for melee attacks
+ * @param {string} [props.hitReroll.ballistic] - hit reroll option for ballistic attacks
  * @returns {object} hitProbabilityReturn
  */
 function hitProbability (props) {
   /**
    * @namespace
-   * @property {object} hitProbabilityReturn - the hitProbability return object
-   * @property {number} hitProbabilityReturn.melee - the melee hit probability
-   * @property {number} hitProbabilityReturn.ballistic - the ballistic hit probability
+   * @property {object} hitProbabilityReturn - hitProbability return object
+   * @property {number} hitProbabilityReturn.melee - melee hit probability
+   * @property {number} hitProbabilityReturn.ballistic - ballistic hit probability
    */
   return {}
 }
