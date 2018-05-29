70cafa4e6ce3675397a4a53924b56c0f28261719
Simon Breiter <hello@simonbreiter.com>
2018-05-29 23:47:29 +0200

    docs: add more parameter, improve name of return object

--- src/damageProbability.js
+++ src/damageProbability.js
@@ -1,20 +1,27 @@
 /**
  * Calculate damage probability
  * @param {object} props - property object
+ * @param {object} props.model - model object
+ * @param {object} props.model.melee - melee object
+ * @param {number} props.model.melee.damage- melee damage
+ * @param {number} props.model.melee.attackPower - melee attackPower
+ * @param {object} props.model.ballistic - ballistic object
+ * @param {number} props.model.ballistic.damage - ballistic damage
+ * @param {number} props.model.ballistic.attackPower - ballistic attackPower
  * @param {object} props.enemy - enemy object
  * @param {number} props.enemy.save - enemy save
  * @param {number} props.enemy.invulnerableSave - enemy invulnerable save
  * @param {object} props.woundProbability - wound probability object
  * @param {number} props.woundProbability.melee - melee wound probability
  * @param {number} props.woundProbability.ballistic - ballistic wound probability
- * @returns {object} damageProbabilityReturn
+ * @returns {object} averageDamage
  */
 function damageProbability (props) {
   /**
    * @namespace
-   * @property {object} damageProbabilityReturn - damageProbability return object
-   * @property {number} damageProbabilityReturn.melee - melee damage probability
-   * @property {number} damageProbabilityReturn.ballistic - ballistic damage probability
+   * @property {object} averageDamage - averageDamage return object
+   * @property {number} averageDamage.melee - average melee damage
+   * @property {number} averageDamage .ballistic - average ballistic damage
    */
   return {}
 }
