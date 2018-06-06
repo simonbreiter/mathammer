08e372f47e180ee4398440d4d4749d416f838b23
carstenkueperkoch <c.kueperkoch@hotmail.com>
2018-06-06 19:47:30 +0200

    feat: error
    an update to add errors for attackPower, damage, save and invulnerableSave

--- test/error.test.js
+++ test/error.test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { errorRange, errorHitType, errorStrength, errorStrengthType, errorToughness, errorToughnessType } from '../src/util/error'
+import { errorRange, errorHitType, errorStrength, errorStrengthType, errorToughness, errorToughnessType, errorSave, errorSaveType, errorAP, errorAPType, errorDamage, errorDamageType } from '../src/util/error'
 
 test('Test Hit Value Range', () => {
   expect(() => { errorRange() }).toThrow('Values are not in range, use a value between 2 and 6.')
@@ -23,4 +23,27 @@ test('Negativ Toughness Error', () => {
 
 test('Test Toughness Value Type', () => {
   expect(() => { errorToughnessType() }).toThrow('Please use a value between 2 and 8.')
+})
+
+test('Save + invulnerable Safe Error', () => {
+  expect(() => { errorSave() }).toThrow('Values are not in range, use a value between 2 and 6.')
+})
+
+test('Test Save + invulnerable Safe Value Type', () => {
+  expect(() => { errorSaveType() }).toThrow('Please use a value between 2 and 6.')
+})
+
+test('Test AP Error', () => {
+  expect(() => { errorAP() }).toThrow('Values are not in range, use a value between 0 and 6.')
+})
+
+test('Test AP Type', () => {
+  expect(() => { errorAPType() }).toThrow('Please use a value between 0 and 6.')
+})
+test('Test DMG Error', () => {
+  expect(() => { errorDamage() }).toThrow('Values are not in range, use a value between 1 and 6.')
+})
+
+test('Test DMG Type', () => {
+  expect(() => { errorDamageType() }).toThrow('Please use a value between 1 and 6.')
 })
